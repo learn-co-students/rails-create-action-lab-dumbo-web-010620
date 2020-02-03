6a27c3f0865b6e5176e1c10034346504c6161069
github username <github email address>
2020-02-03 18:07:34 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,17 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new 
   end
 
   def create
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    # need to cross ref this with the form responsible for generating params
+    # also I think we did this before in a stepwise fashion, using Student.new not create
+    # it looks like it works however!
+
+    redirect_to @student
+    # student_path
   end
 
 end
